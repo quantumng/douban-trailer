411dac51ffa7d3311f0718fbdffd48c487118fe4
吴晓权 <somerq@qq.com>
2019-03-21 20:42:21 +0800

    feature:使用pug构建网站首页

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const Koa = require('koa')
+const views = require('koa-views')
+const { resolve } = require('path')
 const app = new Koa()
-const { normal } = require('./tpl')
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+})) 
+// const { normal } = require('./tpl')
 app.use(async (ctx, next) => {
-  ctx.type = 'text/html;charset=utf-8'
-  ctx.body = normal
+  await ctx.render('index', {
+    you: 'Luke',
+    me: 'SomerQ'
+  })
 })
 app.listen(3000)
