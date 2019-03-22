9e8a0dc00813974786a17033e40bb460f5585169
吴晓权 <somerq@qq.com>
2019-03-22 11:14:41 +0800

    feature:添加爬虫及数据库

---
+++ server/database/init.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const db = 'mongodb://localhost/douban-trailer'
+
+mongoose.Promise = global.Promise
+
+exports.connect = () => {
+  let maxConnectTimes = 0
+  return new Promise((resolve, reject) => {
+    if (process.env.NODE_ENV !== 'production') {
+      mongoose.set('debug', true)
+    }
+    mongoose.connect(db)
+    mongoose.connection.on('disconnected', () => {
+      maxConnectTimes++
+      if (maxConnectTimes < 5) {
+        mongoose.connect(db)
+      } else {
+        throw new Error('数据库挂了')
+      }
+    })
+    mongoose.connection.on('error', err => {
+      maxConnectTimes++
+      console.log(err)
+      if (maxConnectTimes < 5) {
+        mongoose.connect(db)
+      } else {
+        throw new Error('数据库挂了')
+      }
+    })
+    mongoose.connection.once('open', () => {
+      // const Dog = mongoose.model('Dog', { name: String})
+      // const dogge = new Dog({name: '狗头'})
+      // dogge.save().then(() => {
+      //   console.log('wang')
+      // })
+      resolve()
+      console.log('MongoDB is connected')
+    })
+  })
+}
